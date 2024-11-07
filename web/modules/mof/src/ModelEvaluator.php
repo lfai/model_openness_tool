fc5f03b2fcc99f6f1f177c5c7911c34be969d846
Arnaud J Le Hors <lehors@us.ibm.com>
2024-11-07 18:32:26 +0100

    Update class names
    Signed-off-by: Arnaud J Le Hors <lehors@us.ibm.com>

--- web/modules/mof/src/ModelEvaluator.php
+++ web/modules/mof/src/ModelEvaluator.php
@@ -48,13 +48,13 @@ final class ModelEvaluator implements ModelEvaluatorInterface {
       return $this->t('Unclassified');
 
     case 1:
-      return $this->t('Class I - Open Science');
+      return $this->t('Class I - Open Science Model');
 
     case 2:
-      return $this->t('Class II - Open Tooling');
+      return $this->t('Class II - Open Tooling Model');
 
     case 3:
-      return $this->t('Class III - Open Model');
+      return $this->t('Class III - Open Source Model');
 
     case -1:
       return $this->t('Pending evaluation');
