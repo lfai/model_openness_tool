ae082bd1ae8dc5d75cd0c36c091106c59affbb7f
Greg <greg@web.net>
2025-03-11 20:39:19 -0400

    Remove empty line.

--- scripts/sync_licenses.php
+++ scripts/sync_licenses.php
@@ -4,7 +4,6 @@ try {
   print "Syncing licenses..." . PHP_EOL;
   \Drupal::service('license_importer')->import();
   print "Done." . PHP_EOL;
-
 }
 catch (\Drupal\Core\Entity\EntityStorageException $e) {
   print $e->getMessage() . PHP_EOL;
